5b4a999f83fd20a2fab415b18bc86212a36c23c7
Natharam <natharamsoyla701@gmail.com>
2022-12-12 20:01:17 +0530

    product detail page update

---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DetailPageContent from '../components/DetailPageContent';
+
+function ProductDetail() {
+  return (
+    <div className='container mx-auto mt-5'>
+      <DetailPageContent />
+    </div>
+  );
+}
+
+export default ProductDetail;
