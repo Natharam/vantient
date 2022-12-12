84109a55600cd8687a7b3544cced38b2ac21684b
Natharam <natharamsoyla701@gmail.com>
2022-12-12 20:05:25 +0530

    firebase config added

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,32 +1,20 @@
-import firebase from 'firebase/compat/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+// Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDHE9fVBUM_mto-p_SkWnyKtOiRu8M5F98',
-  authDomain: 'react-firebase-farazamiruddin.firebaseapp.com',
-  databaseURL: 'https://react-firebase-farazamiruddin.firebaseio.com',
-  projectId: 'react-firebase-farazamiruddin',
-  storageBucket: 'react-firebase-farazamiruddin.appspot.com',
-  messagingSenderId: '338564911587',
-  appId: '1:338564911587:web:c34e6fee0ff41bbe7fd0d6'
+  apiKey: "AIzaSyAXXvkq-izMBJHhpAnSXSAk-Qy5DjhFlN4",
+  authDomain: "vantient-817f1.firebaseapp.com",
+  databaseURL: "https://vantient-817f1-default-rtdb.firebaseio.com",
+  projectId: "vantient-817f1",
+  storageBucket: "vantient-817f1.appspot.com",
+  messagingSenderId: "920065241168",
+  appId: "1:920065241168:web:610a5e4a6e29932e066155",
+  measurementId: "G-BB1BDVM8YL"
 };
 
-const app = firebase.apps.length > 0 ? firebase.app() : firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-onAuthStateChanged(auth, (user) => {
-  // Check for user status
-  console.log(user, 'user');
-});
-
-const db = getFirestore(app);
-const storage = app.storage();
-const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-
-const EmailProvider = new firebase.auth.EmailAuthProvider();
-
-export { auth, db, storage, timestamp, EmailProvider };
+export const db = getFirestore(app);
+export default app;
