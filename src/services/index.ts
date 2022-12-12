54c7f10dac861887f9f57e93e795375265526de6
Natharam <natharamsoyla701@gmail.com>
2022-12-12 20:06:23 +0530

    helpers added

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,28 @@
-export const getData = () => {}
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { db } from '../config/firebase';
+
+export const getProducts = async (db: any) => {
+  const productsCol = collection(db, 'products');
+  const productsSnapshot = await getDocs(productsCol);
+
+  const productsList = productsSnapshot.docs.map((doc: { data: () => any }) => doc.data());
+  return productsList;
+};
+
+export const addProduct = async (product: any) => {
+  try {
+    const docRef = await addDoc(collection(db, 'products'), product);
+    console.log('Product added with ID: ', docRef);
+  } catch (e) {
+    console.error('Error adding product: ', e);
+  }
+};
+
+export const createUser = async (user: any) => {
+  try {
+    const docRef = await addDoc(collection(db, 'users'), user);
+    console.log('User created with ID: ', docRef);
+  } catch (e) {
+    console.error('Error adding user: ', e);
+  }
+};
