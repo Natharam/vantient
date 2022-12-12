1da96b8ae6f21fcf378564b18b3a86f77612419f
Natharam <natharamsoyla701@gmail.com>
2022-12-12 20:01:44 +0530

    product filters added

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { FaSearch, FaListUl, FaAngleDown, FaPlus } from 'react-icons/fa';
+import { selectedI } from '../utils/types';
 
-function Filters() {
+function Filters({
+  filters,
+  addFilterHandler,
+  onClickHandler
+}: {
+  filters: selectedI[];
+  addFilterHandler: Function;
+  onClickHandler: Function;
+}) {
   return (
-    <div>Filters</div>
-  )
+    <>
+      <div className="flex justify-between items-center mt-4">
+        <div className="flex justify-start items-center">
+          <div className="py-1 mr-3 cursor-pointer">Gallery</div>
+          <div className="mr-3 py-1 px-2 cursor-pointer">Internal listing</div>
+          <div className="py-2 px-2 cursor-pointer">Table</div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="mr-3 py-3 px-2 cursor-pointer">Filter</div>
+          <div className="mr-3 py-3 px-2 cursor-pointer">Sort</div>
+          <div className="py-3 px-2 cursor-pointer">
+            <FaSearch />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-start items-center">
+        {filters.map((filter) => (
+          <div
+            className="flex justify-start items-center py-1 px-3 border-2 mr-2 rounded-full"
+            id="menu-button"
+            // aria-expanded="true"
+            aria-haspopup="true"
+            onClick={() => onClickHandler(filter)}
+          >
+            <FaListUl /> <div className="px-2 cursor-pointer">{filter.label}</div> <FaAngleDown />
+          </div>
+        ))}
+        <div
+          onClick={() => addFilterHandler()}
+          className="flex justify-start items-center hover:bg-gray-200 py-1 px-3 rounded-full cursor-pointer"
+        >
+          <FaPlus /> <div className="ml-2">Add filters</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Filters
+export default Filters;
