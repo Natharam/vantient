0baf5b9cf38e023749eea688225bc8f1c3e10110
Natharam <natharamsoyla701@gmail.com>
2022-12-12 19:59:13 +0530

    product reducer added

--- src/redux/productSlice.ts
+++ src/redux/productSlice.ts
@@ -1,31 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface ProductsSlice {
-  value: number;
+  products: any[];
+  productCount: number;
 }
 
 const initialState: ProductsSlice = {
-  value: 0
+  products: [],
+  productCount: 0,
 };
 
 export const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
+    setProducts: (state, action) => {
+      state.products = action.payload.products;
+      state.productCount = action.payload.productCount;
     }
   }
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = productsSlice.actions;
+export const { setProducts } = productsSlice.actions;
 
 export default productsSlice.reducer;
